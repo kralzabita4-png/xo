eeaef03bbdc235fa592486a291a7bb8c1848f269
kralzabita4-png <kralzabita4@gmail.com>
2025-11-10 11:22:32 +0300

    Update __init__.py

--- HasiiMusic/plugins/__init__.py
+++ HasiiMusic/plugins/__init__.py
@@ -1,19 +1,16 @@
 import glob
-from os.path import dirname, isfile
-
+from os.path import dirname, isfile, join
 
 def __list_all_modules():
     work_dir = dirname(__file__)
-    mod_paths = glob.glob(work_dir + "/*/*.py")
+    py_files = glob.glob(join(work_dir, "**", "*.py"), recursive=True)
 
     all_modules = [
-        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
-        for f in mod_paths
-        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+        f.replace(work_dir + "/", "").replace("/", ".")[:-3]
+        for f in py_files
+        if isfile(f) and not f.endswith("__init__.py")
     ]
-
     return all_modules
-
 
 ALL_MODULES = sorted(__list_all_modules())
 __all__ = ALL_MODULES + ["ALL_MODULES"]
