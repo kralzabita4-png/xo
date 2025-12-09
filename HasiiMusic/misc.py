f798ed189a2e4f25b2ee1a2842baf7eca1a1ce62
kralzabita4-png <kralzabita4@gmail.com>
2025-12-09 13:19:51 +0300

    Update misc.py

--- HasiiMusic/misc.py
+++ HasiiMusic/misc.py
@@ -1,16 +1,13 @@
 import socket
 import time
-
 import heroku3
 from pyrogram import filters
 
 import config
 from HasiiMusic.core.mongo import mongodb
-
 from HasiiMusic.logging import LOGGER
 
 SUDOERS = filters.user()
-
 HAPP = None
 _boot_ = time.time()
 
@@ -19,35 +16,20 @@ def is_heroku():
     return "heroku" in socket.getfqdn()
 
 
-XCB = [
-    "/",
-    "@",
-    ".",
-    "com",
-    ":",
-    "git",
-    "heroku",
-    "push",
-    str(config.HEROKU_API_KEY),
-    "https",
-    str(config.HEROKU_APP_NAME),
-    "HEAD",
-    "master",
-]
-
-
 def dbb():
     global db
     db = {}
-    LOGGER(__name__).info(f"ᴅᴀᴛᴀʙᴀsᴇ ʟᴏᴀᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ💗")
+    LOGGER.info("ᴅᴀᴛᴀʙᴀsᴇ ʟᴏᴀᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ 💗")
 
 
 async def sudo():
     global SUDOERS
     SUDOERS.add(config.OWNER_ID)
+
     sudoersdb = mongodb.sudoers
-    sudoers = await sudoersdb.find_one({"sudo": "sudo"})
-    sudoers = [] if not sudoers else sudoers["sudoers"]
+    data = await sudoersdb.find_one({"sudo": "sudo"})
+    sudoers = [] if not data else data.get("sudoers", [])
+
     if config.OWNER_ID not in sudoers:
         sudoers.append(config.OWNER_ID)
         await sudoersdb.update_one(
@@ -55,21 +37,20 @@ async def sudo():
             {"$set": {"sudoers": sudoers}},
             upsert=True,
         )
-    if sudoers:
-        for user_id in sudoers:
-            SUDOERS.add(user_id)
-    LOGGER(__name__).info(f"sᴜᴅᴏ ᴜsᴇʀs ᴅᴏɴᴇ..")
+
+    for user_id in sudoers:
+        SUDOERS.add(user_id)
+
+    LOGGER.info("sᴜᴅᴏ ᴜsᴇʀs ʟᴏᴀᴅᴇᴅ ✅")
 
 
 def heroku():
     global HAPP
-    if is_heroku:
+    if is_heroku():
         if config.HEROKU_API_KEY and config.HEROKU_APP_NAME:
             try:
-                Heroku = heroku3.from_key(config.HEROKU_API_KEY)
-                HAPP = Heroku.app(config.HEROKU_APP_NAME)
-                LOGGER(__name__).info(f"ʜᴇʀᴏᴋᴜ ᴀᴘᴘ ᴄᴏɴғɪɢᴜʀᴇᴅ..")
-            except BaseException:
-                LOGGER(__name__).warning(
-                    f"ʏᴏᴜ sʜᴏᴜʟᴅ ʜᴀᴠᴇ ɴᴏᴛ ғɪʟʟᴇᴅ ʜᴇʀᴏᴋᴜ ᴀᴘᴘ ɴᴀᴍᴇ ᴏʀ ᴀᴘɪ ᴋᴇʏ ᴄᴏʀʀᴇᴄᴛʟʏ ᴘʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ɪᴛ..."
-                )
+                hk = heroku3.from_key(config.HEROKU_API_KEY)
+                HAPP = hk.app(config.HEROKU_APP_NAME)
+                LOGGER.info("ʜᴇʀᴏᴋᴜ ᴀᴘᴘ ᴄᴏɴғɪɢᴜʀᴇᴅ ✅")
+            except Exception as e:
+                LOGGER.warning(f"ʜᴇʀᴏᴋᴜ ʜᴀᴛᴀ: {e}")
